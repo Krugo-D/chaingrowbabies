f5171cb71108a251d2ffc1015edc9612267f9c70
dogperson <fondofdogs@protonmail.com>
2022-10-10 22:22:49 +0700

    combined deploy scripts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,17 +13,24 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-
-  
+  //deploy token
   const Token = await hre.ethers.getContractFactory("ChainGrowBabiesToken");
   const token = await Token.deploy();
-
-
-
   await token.deployed();
-
   console.log("Token deployed to:", token.address);
+
+  const linkAddress = '0x326C977E6efc84E512bB9C30f76E30c160eD06FB'
+  const oracleAddress = '0xB9756312523826A566e222a34793E414A81c88E1'
+
+  // deploy NFT
+  const NFT = await hre.ethers.getContractFactory("ChainGrowBabiesNFT");
+  const nft = await NFT.deploy(linkAddress, oracleAddress, token.address);
+
+
+
+  await nft.deployed();
+
+  console.log("NFT deployed to:", nft.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
